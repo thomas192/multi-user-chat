ce7364a91ad49bba03479634bb4eb295ff85d777
thomas192 <65908739+thomas192@users.noreply.github.com>
2020-11-27 20:27:28 +0100

    Diffusion du status en ligne

--- POCMultiUserChat/ChatServer/src/com/muc/ServerMain.java
+++ POCMultiUserChat/ChatServer/src/com/muc/ServerMain.java
@@ -1,28 +1,12 @@
 package com.muc;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 public class ServerMain {
     public static void main(String[] args) {
-        /* Port to listen to */
+        // Port to listen to
         int port = 8818;
-        try {
-            // Create server socket that listens on given port
-            ServerSocket serverSocket = new ServerSocket(port);
-            // Continuously accept incoming connections from client
-            while(true) {
-                System.out.println("About to accept client connection...");
-                // Create connection between server and client
-                Socket clientSocket = serverSocket .accept();
-                System.out.println("Accepted connection from" + clientSocket);
-                // Create new server worker instance each we get a new connection from client
-                ServerWorker worker = new ServerWorker(clientSocket);
-                worker.start();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+        Server server = new Server(port);
+        server.start();
+
     }
 }
--- POCMultiUserChat/ChatServer/src/com/muc/ServerWorker.java
+++ POCMultiUserChat/ChatServer/src/com/muc/ServerWorker.java
@@ -4,19 +4,36 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.Date;
+import java.util.List;
 
-/** Thread that handles communication with client socket */
+/** Class that is basically a thread which handles communication with client socket */
 public class ServerWorker extends Thread {
 
+
+    /** Server instance that we can pass to each server worker */
+    private final Server server;
+
+    /** Client socket */
     private final Socket clientSocket;
+
+    /** Stores the output stream of the client socket which allows us to write data for the client */
+    private OutputStream outputStream;
+
+    /** Stores the login of the server worker */
     private String login = null;
 
-    public ServerWorker(Socket clientSocket) {
+    /** Constructor of server worker class */
+    public ServerWorker(Server server, Socket clientSocket) {
+        this.server = server;
         this.clientSocket = clientSocket;
     }
 
-    // When thread runs
+    /** Public getter of the login so that other server workers know what the login is */
+    public String getLogin() {
+        return login;
+    }
+
+    // Overrides the thread run function
     @Override
     public void run() {
         try {
@@ -30,22 +47,25 @@ public class ServerWorker extends Thread {
 
     /** Communicates with client via client socket */
     private void handleClientSocket() throws IOException, InterruptedException {
-        // Allows to read data from client
+        // Store the input stream which allows us to read data from the client
         InputStream inputStream = clientSocket.getInputStream();
-        // Allows to write data for client
-        OutputStream outputStream = clientSocket.getOutputStream();
 
-        // Create buffer so we can read line by line
+        // Initialize the output stream
+        this.outputStream = clientSocket.getOutputStream();
+
+        // Create a buffer to read the input stream line by line
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         String line;
         while ( (line = reader.readLine()) != null) {
             // Split line into individual tokens based on whitespace character
             String[] tokens = StringUtils.split(line);
-            // Check if tokens is valid
+
+            // Check if the tokens list is valid
             if (tokens != null && tokens.length > 0) {
                 // Get first token of line
                 String cmd = tokens[0];
-                // Read token
+
+                // Read token and do something if it's a known command
                 if ("quit".equalsIgnoreCase(cmd)) {
                     break;
                 } else if ("login".equalsIgnoreCase(cmd)) {
@@ -56,7 +76,6 @@ public class ServerWorker extends Thread {
                 }
             }
         }
-
         // Close connection
         clientSocket.close();
     }
@@ -69,17 +88,48 @@ public class ServerWorker extends Thread {
             String password = tokens[2];
 
             String msg;
-            // Hard code login
+            // Hard coded login
             if ((login.equals("guest") && password.equals("guest"))
                     || (login.equals("thomas") && password.equals("thomas"))) {
-                msg = "ok login\n";
+                msg = "You are logged in\n";
                 outputStream.write(msg.getBytes());
                 this.login = login;
                 System.out.println("User logged in successfully: " + login);
+
+                // Get the list of all the workers connected to the server
+                List<ServerWorker> workerList = server.getWorkerList();
+
+                // Send current user all other online logins
+                for (ServerWorker worker : workerList) {
+                    // Check if we are not sending presence of not connected users yet
+                    if (worker.getLogin() != null) {
+                        if (!login.equals(worker.getLogin())) {
+                            msg = worker.getLogin() + " is online\n";
+                            send(msg);
+                        }
+                    }
+                }
+
+                // Send other online users current user's status
+                for (ServerWorker worker : workerList) {
+                    // Check if we are not sending our own presence
+                    if (!login.equals(worker.getLogin())) {
+                        msg = login + " is online\n";
+                        worker.send(msg);
+                    }
+                }
+
             } else {
                 msg = "error login\n";
                 outputStream.write(msg.getBytes());
             }
+        }
+    }
+
+    /** Sends a message to a client using his output stream */
+    private void send(String msg) throws IOException {
+        if (login != null) {
+            outputStream.write(msg.getBytes());
         }
     }
 }
