7b5e473132decd1b6f79e00a1e39c91e1174cefa
thomas192 <65908739+thomas192@users.noreply.github.com>
2020-12-04 18:29:02 +0100

    Messagerie de groupe

--- POCMultiUserChat/ChatServer/src/com/muc/ServerWorker.java
+++ POCMultiUserChat/ChatServer/src/com/muc/ServerWorker.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.HashSet;
 import java.util.List;
 
 /** Class that is basically a thread which handles communication with client socket */
@@ -21,6 +22,9 @@ public class ServerWorker extends Thread {
 
     /** Stores the login of the server worker */
     private String login = null;
+
+    /** Stores the membership of the user to a topic */
+    private HashSet<String> topicSet = new HashSet<>();
 
     /** Constructor of server worker class */
     public ServerWorker(Server server, Socket clientSocket) {
@@ -64,7 +68,6 @@ public class ServerWorker extends Thread {
             if (tokens != null && tokens.length > 0) {
                 // Get first token of line
                 String cmd = tokens[0];
-
                 // Read token and do something if it's a known command
                 // Disconnection
                 if ("logoff".equals(cmd) || "quit".equalsIgnoreCase(cmd)) {
@@ -73,11 +76,17 @@ public class ServerWorker extends Thread {
                 // Connection
                 } else if ("login".equalsIgnoreCase(cmd)) {
                     handleLogin(outputStream, tokens);
-                // Message someone
+                // User to user messaging
                 } else if ("msg".equalsIgnoreCase(cmd)) {
                     // Split line into 3 tokens (3rd token is the message)
                     String[] tokensMsg = StringUtils.split(line, null, 3);
                     handleMessage(tokensMsg);
+                // Join topic
+                } else if ("join".equalsIgnoreCase(cmd)) {
+                    handleJoin(tokens);
+                // Leave topic
+                } else if ("leave".equalsIgnoreCase((cmd))) {
+                    handleLeave(tokens);
                 // Unknown command
                 } else {
                     String msg = "unknown command " + cmd + "\n";
@@ -89,20 +98,53 @@ public class ServerWorker extends Thread {
         clientSocket.close();
     }
 
-    /** Handles message (format: "msg" "login" body) */
+    private void handleLeave(String[] tokens) {
+        if (tokens.length > 1) {
+            String topic = tokens[1];
+            // This connection has joined this topic
+            topicSet.remove(topic);
+        }
+    }
+
+    /** Checks if user is subscribed to given topic */
+    public boolean isMemberOfTopic(String topic) {
+        return topicSet.contains(topic);
+    }
+
+    private void handleJoin(String[] tokens) {
+        if (tokens.length > 1) {
+            String topic = tokens[1];
+            // This connection has joined this topic
+            topicSet.add(topic);
+        }
+    }
+
+    /** Handles message
+     * format: "msg" "login" body
+     * format: "msg" "#topic" body */
     private void handleMessage(String[] tokens) throws IOException {
         // Message recipient
         String sendTo = tokens[1];
         // Message to send
         String body = tokens[2];
+        // Determine whether 2nd token is a topic
+        boolean isTopic = sendTo.charAt(0) == '#';
         // Get the list of all the workers connected to the server
         List<ServerWorker> workerList = server.getWorkerList();
         for(ServerWorker worker : workerList) {
-            // If worker matches recipient
-            if (sendTo.equalsIgnoreCase(worker.getLogin())) {
-                // Display message
-                String outMsg = login + ": " + body + "\n";
-                worker.send(outMsg);
+            if (isTopic) {
+                // Check if user is subscribed to the topic
+                if (worker.isMemberOfTopic(sendTo)) {
+                    String outMsg = sendTo + ":" + login + ": " + body + "\n";
+                    worker.send(outMsg);
+                }
+            } else {
+                // If worker matches recipient
+                if (sendTo.equalsIgnoreCase(worker.getLogin())) {
+                    // Display message
+                    String outMsg = login + ": " + body + "\n";
+                    worker.send(outMsg);
+                }
             }
         }
     }
