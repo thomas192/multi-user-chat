9420397e22c99a0af092ff4039cf1d0aa75b7146
thomas192 <65908739+thomas192@users.noreply.github.com>
2020-12-06 22:20:32 +0100

    Message UI (dans une nouvelle fenêtre)

---
+++ POCMultiUserChat/ChatClient/src/com/muc/MessagePane.java
@@ -0,0 +1,66 @@
+package com.muc;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+public class MessagePane extends JPanel implements MessageListener {
+
+    /** Chat client (opened the message pane) */
+    private final ChatClient client;
+
+    /** List model of conversation's messages */
+    private DefaultListModel<String> listModel = new DefaultListModel<>();
+    /** Stores the messages of the current conversation */
+    private JList<String> messageList = new JList<>(listModel);
+
+    /** Input field for messages */
+    private JTextField inputField = new JTextField();
+
+    /** Login of user chat client is going to send messages to */
+    private final String login;
+
+    public MessagePane(ChatClient client, String login) {
+        this.client = client;
+        this.login = login;
+
+        // When the other user sends the client a message
+        client.addMessageListener(this);
+
+        setLayout(new BorderLayout());
+        // Add the message list UI as the center component
+        add(new JScrollPane(messageList), BorderLayout.CENTER);
+        // Add the input field as the bottom component
+        add(inputField, BorderLayout.SOUTH);
+        // Listener of the message field
+        inputField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    // Get the typed message
+                    String message = inputField.getText();
+                    // Send the message using client API
+                    client.msg(login, message);
+                    // Add the message to the conversation
+                    listModel.addElement("You: " + message);
+                    // Reset text field
+                    inputField.setText("");
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onMessage(String fromLogin, String msgBody) {
+        // Check if the message is intended for this message pane instance
+        if (login.equalsIgnoreCase(fromLogin)) {
+            // Add received message to the conversation
+            String message = fromLogin + ": " + msgBody;
+            listModel.addElement(message);
+        }
+    }
+}
--- POCMultiUserChat/ChatClient/src/com/muc/UserListPane.java
+++ POCMultiUserChat/ChatClient/src/com/muc/UserListPane.java
@@ -1,8 +1,9 @@
 package com.muc;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 
 public class UserListPane extends JPanel implements UserStatusListener {
@@ -13,18 +14,40 @@ public class UserListPane extends JPanel implements UserStatusListener {
     /** Shows the list of connected users */
     private JList<String> userListUI;
 
+    /** List model of connected users */
     private DefaultListModel<String> userListModel;
 
     public UserListPane(ChatClient client) {
         this.client = client;
+
         // Add presence listener
         this.client.addUserStatusListener(this);
-        // Instantiate list model
+
         userListModel = new DefaultListModel<>();
-        // Instantiate list component
         userListUI = new JList<>(userListModel);
         setLayout(new BorderLayout());
         add(new JScrollPane(userListUI), BorderLayout.CENTER);
+
+        // Listener of the connected users list
+        userListUI.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // Check if it's a double click
+                if (e.getClickCount() > 1) {
+                    // Get clicked user's login
+                    String login = userListUI.getSelectedValue();
+                    // Create a message pane for that login
+                    MessagePane messagePane = new MessagePane(client, login);
+                    // Show the message pane in a separate window
+                    JFrame f = new JFrame("Message: " + login);
+                    f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                    f.setSize(500, 500);
+                    // Add the message pane as the center component
+                    f.getContentPane().add(messagePane, BorderLayout.CENTER);
+                    f.setVisible(true);
+                }
+            }
+        });
 
         // Check if connection to server is successful
         if (client.connect()) {
@@ -38,13 +61,13 @@ public class UserListPane extends JPanel implements UserStatusListener {
 
     public static void main(String[] args) {
         ChatClient client = new ChatClient("localhost", 8818);
-
+        // Create user pane that takes the client
         UserListPane userListPane = new UserListPane(client);
         // Create window
         JFrame frame = new JFrame("User List");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 600);
-        // Set userListPane as the main component
+        // Add the user list pane as the main component
         frame.getContentPane().add(userListPane, BorderLayout.CENTER);
         frame.setVisible(true);
     }
