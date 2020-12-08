04689a98366313b66817f1721a7d15f3fea7e55c
thomas192 <65908739+thomas192@users.noreply.github.com>
2020-12-08 18:21:02 +0100

    Ajout de getTopicsFollowed() au DAO

--- POCMultiUserChat/ChatServer/src/poc/DAO.java
+++ POCMultiUserChat/ChatServer/src/poc/DAO.java
@@ -1,34 +1,54 @@
 package poc;
 
 import java.sql.*;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class DAO {
+    private final Connection connection;
+
     public DAO() {
+        this.connection = Database.getInstance().getConnection();
     }
 
     public static void main(String[] args) {
         DAO dao = new DAO();
-        boolean res = dao.connectUser("thomas", "thomas");
+        HashSet<String> res = dao.getTopicsFollowed("thomas");
         System.out.println(res);
     }
 
     public boolean connectUser(String login, String password) {
-        Connection connection = Database.getInstance().getConnection();
-        PreparedStatement requestUser;
         try {
-            requestUser = connection.prepareStatement("SELECT * FROM chatuser WHERE login = ? AND password = ?" );
-            requestUser.setString(1, login);
-            requestUser.setString(2, password);
+            PreparedStatement query = connection.prepareStatement("SELECT * FROM chatuser WHERE login = ? AND password = ?" );
+            query.setString(1, login);
+            query.setString(2, password);
 
-            ResultSet resultSet = requestUser.executeQuery();
+            ResultSet rs = query.executeQuery();
 
-            if (!resultSet.next()) {
+            if (!rs.next())
                 return false;
-            }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
         return true;
+    }
+
+    public HashSet<String> getTopicsFollowed(String login) {
+        HashSet<String> topicsFollowed = null;
+        try {
+            PreparedStatement query = connection.prepareStatement("SELECT topics FROM topicsfollowed WHERE login = ?");
+            query.setString(1, login);
+
+            ResultSet rs = query.executeQuery();
+            rs.next();
+            Array a = rs.getArray("topics");
+
+            topicsFollowed = Arrays.stream((String[]) a.getArray()).collect(Collectors.toCollection(HashSet::new));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return topicsFollowed;
     }
 }
