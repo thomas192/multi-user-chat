e4182f8ca4d4d45a8d37aa547d7a9118c13c7483
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2020-12-12 13:15:09 +0100

    Ajout du modele TopicMessage

---
+++ POCMultiUserChat/ChatClient/src/poc/modele/TopicMessage.java
@@ -0,0 +1,22 @@
+package poc.modele;
+
+public class TopicMessage {
+    protected String body;
+    protected String sender;
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
